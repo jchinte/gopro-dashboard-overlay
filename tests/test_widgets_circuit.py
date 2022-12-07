d9d01415ac29ed02a44baad01feb5cf6d76c7e8e
James Richardson <james@time4tea.net>
2022-12-07 17:56:17 +0000

    try out an instrument in cairo

--- tests/test_widgets_circuit.py
+++ tests/test_widgets_circuit.py
@@ -1,14 +1,9 @@
 import random
 from datetime import timedelta
 
-import pytest
-
 from gopro_overlay import fake
 from gopro_overlay.dimensions import Dimension
 from gopro_overlay.privacy import NoPrivacyZone
-from gopro_overlay.widgets.cairo import circuit
-from gopro_overlay.widgets.cairo.cairo import CairoWidget
-from gopro_overlay.widgets.cairo.circuit import CairoCircuit, Line
 from gopro_overlay.widgets.map import Circuit
 from tests.approval import approve_image
 from tests.test_widgets import time_rendering
@@ -19,62 +14,6 @@ rng.seed(12345)
 ts = fake.fake_framemeta(timedelta(minutes=10), step=timedelta(seconds=1), rng=rng)
 
 
-def cairo_widget_test(widgets, rotation=0, repeat=1):
-    return time_rendering(
-        name="test_gauge",
-        dimensions=Dimension(500, 500),
-        widgets=[
-            CairoWidget(size=Dimension(500, 500), rotation=rotation, widgets=widgets)
-        ],
-        repeat=repeat
-    )
-
-
-def pt():
-    return ts.get(ts.min).point
-
-
-@pytest.mark.gfx
-@approve_image
-def test_cairo_circuit_defaults():
-    return cairo_widget_test([CairoCircuit(framemeta=ts, location=pt)])
-
-
-@pytest.mark.gfx
-@approve_image
-def test_cairo_circuit_defaults_rotate():
-    return cairo_widget_test([CairoCircuit(framemeta=ts, location=pt)], rotation=45)
-
-
-@pytest.mark.gfx
-@approve_image
-def test_cairo_circuit_line_width():
-    return cairo_widget_test(
-        [CairoCircuit(framemeta=ts, location=pt, line=Line(circuit.black, circuit.white, width=0.05))])
-
-
-@pytest.mark.gfx
-@approve_image
-def test_cairo_circuit_fill():
-    return cairo_widget_test(
-        [CairoCircuit(framemeta=ts, location=pt, line=Line((255, 0, 255), circuit.white, width=0.01))])
-
-
-@pytest.mark.gfx
-@approve_image
-def test_cairo_circuit_outline():
-    return cairo_widget_test(
-        [CairoCircuit(framemeta=ts, location=pt, line=Line(circuit.black, (255, 0, 255), width=0.01))])
-
-
-@pytest.mark.gfx
-@approve_image
-def test_cairo_circuit_location():
-    return cairo_widget_test(
-        [CairoCircuit(framemeta=ts, location=pt, loc=Line(circuit.blue, circuit.white, width=0.025))])
-
-
-@pytest.mark.gfx
 @approve_image
 def test_circuit():
     return time_rendering(
